1fc739e23eba91274d9f6c0d582c2f9a908c276e
Avneet Singh <avneetmaankiya@gmail.com>
2023-12-19 04:02:26 +0530

    viewModel and models added

---
+++ Todo App IOS/Models/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  Todo App IOS
+//
+//  Created by Avi manak on 12/12/23.
+//
+
+import Foundation
---
+++ Todo App IOS/ViewModels/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  Todo App IOS
+//
+//  Created by Avi manak on 19/12/23.
+//
+
+import Foundation
